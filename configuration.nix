8b778e1fb784559a4c932fa198d4c157a62fb4c7
Parikshit Nadimpalli <109894397+parikshitnadimpalli@users.noreply.github.com>
2026-01-13 01:34:35 +0530

    Modify locale and user settings in configuration.nix
    Updated locale settings and added extra local settings for Indian standards. Adjusted user and service configurations.

--- configuration.nix
+++ configuration.nix
@@ -8,27 +8,39 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
   time.timeZone = "Asia/Kolkata";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocalSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  }
 
-  services.printing.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+  servicer.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  }
 
   users.users.parikshit = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
+    packages = with pkgs; [];
   };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
     vim
